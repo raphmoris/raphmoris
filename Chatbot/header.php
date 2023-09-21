f88a1e37d937959c49eac68354ccd4668f08af84
RAPH MORIS <136050846+raphmoris@users.noreply.github.com>
2023-09-21 19:50:04 +0100

    Update header.php

--- Chatbot/header.php
+++ Chatbot/header.php
@@ -5,7 +5,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Realtime Chat App | CodingNepal</title>
+  <title>Raphmoris ChatApp</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/all.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/brands.min.css">
+  <link rel="stylesheet" href="css/brands.css">
+  <link rel="stylesheet" href="css/solid.css">
+  <link rel="stylesheet" href="css/solid.min.css">
+  <link rel="stylesheet" href="css/regular.min.css">
+  <link rel="stylesheet" href="css/regular.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
